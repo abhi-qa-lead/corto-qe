5b5c95b02294e6537a050fd04bb7f09643bee85e
Abhi D <30851622+abhidp@users.noreply.github.com>
2026-04-05 00:51:46 +1100

    COR-22: Setup Github Actions for UI Tests (#10)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test';
+import type { GitHubActionOptions } from '@estruyf/github-actions-reporter';
 
 export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [
+        ['list'],
+        ['html'],
+        [
+          '@estruyf/github-actions-reporter',
+          <GitHubActionOptions>{
+            title: 'Corto QE UI Test Results',
+            useDetails: true,
+            showError: true,
+          },
+        ],
+      ]
+    : [['list'], ['html']],
   use: {
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
@@ -27,4 +41,4 @@ export default defineConfig({
       },
     },
   ],
-})
+});
